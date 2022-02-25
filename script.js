84b1ade224d3d1b791a3929ce8042ff6c4e919b3
Aditya_programmer <75136463+Aditya121-bot@users.noreply.github.com>
2022-02-25 22:11:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,33 @@ window.onscroll= function(){
     var a=window.pageYOffset;
     if (pr>a){
                     
-                   document.getElementById("NavBottom").style="bottom:0px;"        
-         document.getElementById("NavTop").style="top:0px;"        
-   document.getElementById("profile_top_nav").style="border-bottom:0px solid black;"                          
-                 
+                   document.getElementById("search_nav").style="top:0px;"        
+        
+document.getElementById("search_nav").style.transition = "all .5s";         document.getElementById("NavTop").style="top:0px;"        
+   document.getElementById("profile_top_nav").style="border-bottom:.7px solid white;"                          
+  
+  document.getElementById("notification_tab").style="border-bottom:0px solid #CACCCE;"                     
     }
-    else if(pr<a && pr>7){
+    else if(pr<a && pr>15){
                       
            document.getElementById("NavBottom").style="border-top-color:red;"
-             document.getElementById("NavBottom").style="box-shadow:1px 0px 1px 0px grey;"
-                    document.getElementById("NavTop").style="border-bottom-color:grey;"      
-                     document.getElementById("NavBottom").style="bottom:0px;"        
-       document.getElementById("profile_top_nav").style="border-bottom:.2px solid black;"                  
+             document.getElementById("NavBottom").style="box-shadow:1px 0px 1px 0px #CACCCE;"
+                    document.getElementById("NavTop").style="box-shadow: 0px 1.5px 1.5px -1.5px black;"              
+ document.getElementById("NavTop").style="border-bottom:.7px solid #CACCCE;"          
                       
+ 
+ document.getElementById("search_nav").style="top:-100px;"        
+        
+ document.getElementById("search_nav").style.transition = "all .6s";       
+                    
+                     document.getElementById("NavBottom").style="bottom:.7px;"        
+document.getElementById("profile_top_nav").style="box-shadow: 0px 1.5px 1.5px -1.5px black;"              
+                      
+                      document.getElementById("profile_top_nav").style="border-bottom:.7px solid #CACCCE;"         
+  
+ document.getElementById("notification_tab").style="border-bottom:.7px solid #CACCCE;"                         
+                      
+                                    
     }
     pr=a;
 }
@@ -29,4 +43,7 @@ function tabs(tabc) {
     }
     document.getElementById(tabc).style.display = "block";  
 }
-//tab active
+//change user_name 
+
+
+
